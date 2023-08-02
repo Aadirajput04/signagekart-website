eabc0e123530beba083f2046bdf5df26a305d994
aaditya singh <aadityasingh396@gmail.com>
2023-08-02 10:54:41 +0530

    product-section completed

--- product-section.php
+++ product-section.php
@@ -1,4 +1,4 @@
-<section class="product-main-box">
+<section class="product-main-box side-padding page-width">
 
     <div class="product-head-text">
 
@@ -20,12 +20,73 @@
                 <h3><img src="./imgs/media.png" alt=""> Media Player</h3>
                 <ul>
 
-                    <li>Multimedia play</li>
-                    <li>Individuation</li>
-                    <li>High efficiency</li>
+                    <li>
+                        <p> Multimedia play </p>
+                    </li>
+                    <li>
+                        <p> Individuation </p>
+                    </li>
+                    <li>
+                        <p> High efficiency </p>
+                    </li>
                 </ul>
-                <a href="#"> Buy Now ></a>
+                <a href="#"> Buy Now  ></a>
+                <img class="side-abs" src="./imgs/product-side-img.png" alt="">
 
+
+            </div>
+
+
+
+
+        </div>
+        
+        <div class="product-box">
+
+            <img src="./imgs/product-2.png" alt="">
+            <div class="product-detail">
+                <h3><img src="./imgs/media2.png" alt="">Signage Board</h3>
+                <ul>
+
+                    <li>
+                        <p> Web Based Remote management</p>
+                    </li>
+                    <li>
+                        <p> Individuation </p>
+                    </li>
+                    <li>
+                        <p> High efficiency </p>
+                    </li>
+                </ul>
+                <a href="#"> Buy Now  ></a>
+
+                <img class="side-abs" src="./imgs/product-side-img.png" alt="">
+            </div>
+
+
+
+
+        </div>
+        
+        <div class="product-box">
+
+            <img src="./imgs/product-3.png" alt="">
+            <div class="product-detail">
+                <h3><img src="./imgs/media3.png" alt="">Holograms</h3>
+                <ul>
+
+                    <li>
+                        <p>High Definition </p>
+                    </li>
+                    <li>
+                        <p>High Brightness RGB </p>
+                    </li>
+                    <li>
+                        <p>Lifespan >30000 hours </p>
+                    </li>
+                </ul>
+                <a href="#"> Buy Now  ></a>
+                <img class="side-abs" src="./imgs/product-side-img.png" alt="">
 
             </div>
 
